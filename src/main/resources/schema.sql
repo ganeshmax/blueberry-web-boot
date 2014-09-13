2909bc763bd26c464577aee3cb4f6cade6088da6
ganeshmax <ganeshmax@gmail.com>
2014-09-12 22:52:15 -0400

    heroku

--- src/main/resources/schema.sql
+++
@@ -1,9 +0,0 @@
-# Add DDL statements here once you decide to get away from ddl-auto. Maybe use liquibase or autofly instead.
-# create table users (
-#   id integer not null auto_increment,
-#   email varchar(100) not null,
-#   pwd varchar(100) not null,
-#   phone varchar(100),
-#
-#   primary key (id)
-# );
